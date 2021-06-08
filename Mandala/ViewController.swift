7ec027f0b23d1f9dbb052ee650c31f157e42e8ae
Alberto Silva <silvaguzmanalberto@gmail.com>
2021-06-08 11:30:25 -0500

    Initial Commit

---
+++ Mandala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mandala
+//
+//  Created by Alberto Silva on 08/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
